83fa6301ff80b2d4bf06cea3aa36c467f4a32eac
Alexey Gaidykov <swiftgaida@list.ru>
2023-02-01 19:14:03 +0300

    Initial Commit

---
+++ Don't forget./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Don't forget.
+//
+//  Created by Alexey Gaidykov on 01.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
